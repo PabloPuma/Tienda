63ca1b74d1f354f411d03abe8713948d22d40ff7
Pablo Puma Quicaño <pablopumaq@gmail.com>
2021-06-15 12:19:27 -0500

    tienda de autos

---
+++ src/Controller/AlumnosController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Alumnos;
+use App\Form\AlumnosType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/alumnos")
+ */
+class AlumnosController extends AbstractController
+{
+    /**
+     * @Route("/", name="alumnos_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $alumnos = $this->getDoctrine()
+            ->getRepository(Alumnos::class)
+            ->findAll();
+
+        return $this->render('alumnos/index.html.twig', [
+            'alumnos' => $alumnos,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="alumnos_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $alumno = new Alumnos();
+        $form = $this->createForm(AlumnosType::class, $alumno);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($alumno);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('alumnos_index');
+        }
+
+        return $this->render('alumnos/new.html.twig', [
+            'alumno' => $alumno,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="alumnos_show", methods={"GET"})
+     */
+    public function show(Alumnos $alumno): Response
+    {
+        return $this->render('alumnos/show.html.twig', [
+            'alumno' => $alumno,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="alumnos_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Alumnos $alumno): Response
+    {
+        $form = $this->createForm(AlumnosType::class, $alumno);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('alumnos_index');
+        }
+
+        return $this->render('alumnos/edit.html.twig', [
+            'alumno' => $alumno,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="alumnos_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Alumnos $alumno): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$alumno->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($alumno);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('alumnos_index');
+    }
+}
---
+++ src/Entity/Alumnos.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Alumnos
+ *
+ * @ORM\Table(name="alumnos")
+ * @ORM\Entity
+ */
+class Alumnos
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="nombre", type="string", length=50, nullable=true)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="edad", type="string", length=50, nullable=false)
+     */
+    private $edad;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(?string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getEdad(): ?string
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(string $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+
+}
